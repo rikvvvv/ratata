1709eba405070c34c456cec28ac46ad6ac75cedf
rikvvvv <85257279+rikvvvv@users.noreply.github.com>
2022-04-08 22:47:36 +0200

    roznie zmiany takie ten

---
+++ ratata/JedenGracz.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ratata
+{
+    /// <summary>
+    /// Logika interakcji dla klasy JedenGracz.xaml
+    /// </summary>
+    public partial class JedenGracz : Window
+    {
+        public JedenGracz()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ratata/Menu.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ratata
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Menu.xaml
+    /// </summary>
+    public partial class Menu : Window
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void TwoPlayers(object sender, RoutedEventArgs e)
+        {
+            MainWindow objDwochGraczy = new MainWindow();
+            this.Visibility = Visibility.Hidden;
+            objDwochGraczy.Show();
+        }
+    }
+}
